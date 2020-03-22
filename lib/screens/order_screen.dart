b8cd26774f832dc2142d670c8adb072c5b2c58b3
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-03-22 16:24:32 +0500

    Update

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -6,18 +6,39 @@ import '../widgets/app_drawer.dart';
 
 class OrderScreen extends StatelessWidget {
   static const routeName = '/orders';
+
   @override
   Widget build(BuildContext context) {
-    final orderDate = Provider.of<Orders>(context);
+    print('building orders!');
+    // final orderDate = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
-      body: ListView.builder(
-        itemBuilder: (ctx, i) => OrderItem(
-          orderDate.orders[i],
-        ),
-        itemCount: orderDate.orders.length,
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (dataSnapshot.error != null) {
+              return Center(
+                child: Text('An error occurred'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemBuilder: (ctx, i) => OrderItem(
+                    orderData.orders[i],
+                  ),
+                  itemCount: orderData.orders.length,
+                ),
+              );
+            }
+          }
+        },
       ),
       drawer: AppDrawer(),
     );
