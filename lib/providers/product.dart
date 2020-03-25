c542ee20ce7a1d1a6fe1fc22354d80f9fe0d5e01
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-03-25 20:24:06 +0500

    Updates

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -23,18 +23,19 @@ class Product with ChangeNotifier {
     isFavorite = newValue;
     notifyListeners();
   }
-  Future<void> toggleFavoriteStatus() async {
+
+  Future<void> toggleFavoriteStatus(String token, String userId) async {
     final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
     final url =
-        'https://shop-app-flutter-afdf9.firebaseio.com/products/$id.json';
+        'https://shop-app-flutter-afdf9.firebaseio.com/products/$userId/$id.json?auth=$token';
     try {
-    final response = await http.patch(
+      final response = await http.put(
         url,
-        body: json.encode({
-          'isFavorite': isFavorite,
-        }),
+        body: json.encode(
+          isFavorite,
+        ),
       );
       if (response.statusCode >= 400) {
         _setFavValue(oldStatus);
