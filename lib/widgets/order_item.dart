bb36016ef716173bb9da4f440d32faf2d6c00f90
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-03-27 11:59:19 +0500

    Updates

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -40,7 +40,6 @@ class _OrderItemState extends State<OrderItem> {
             ),
               AnimatedContainer(
                 duration: Duration(milliseconds: 300),
-
                 padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
                 height: _expanded ? min(widget.order.products.length * 20.0 + 10, 100) : 0,
                 child: ListView(
